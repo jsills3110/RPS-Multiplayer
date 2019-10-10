29b18cd7f9b3fbf5ac6e5892d7cb6f3d14188b25
Jessica Sills <jsills3110@gmail.com>
2019-10-09 22:13:48 -0700

    Added images to match the players' choices. Modified the function that changes the wins/losses of the players so that the changes happen in the same request, rather than different requests.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,6 +6,17 @@ var player2Content = $("#player2-content");
 var player1Button = $("#player1-name-button");
 var player2Button = $("#player2-name-button");
 var deckDiv = $("#deck");
+var player1Image = $("#player1-img");
+var player2Image = $("#player2-img");
+var player1Wins = $("#player1-wins");
+var player1Losses = $("#player1-losses");
+var player2Wins = $("#player2-wins");
+var player2Losses = $("#player2-losses");
+
+var buttonBSClasses = "btn m-1";
+var rockImage = "<img src='assets/images/rock.png' class='img-fluid'/>";
+var scissorsImage = "<img src='assets/images/scissors.png' class='img-fluid'/>";
+var paperImage = "<img src='assets/images/paper.png' class='img-fluid'/>";
 
 // Firebase configuration
 var firebaseConfig = {
@@ -86,9 +97,9 @@ player2Button.on("click", function (event) {
 
 // When a player enters their name, show the rock paper scissor options.
 function addRPSButtons(thePlayer) {
-    var rock = $("<button class='btn btn-primary choice" + thePlayer + "' value='rock'>Rock</button>");
-    var paper = $("<button class='btn btn-primary choice" + thePlayer + "' value='paper'>Paper</button>");
-    var scissors = $("<button class='btn btn-primary choice" + thePlayer + "' value='scissors'>Scissors</button>");
+    var rock = $("<button class='btn-dark " + buttonBSClasses + " choice" + thePlayer + "' value='rock'>Rock</button>");
+    var paper = $("<button class='btn-secondary " + buttonBSClasses + " choice" + thePlayer + "' value='paper'>Paper</button>");
+    var scissors = $("<button class='btn-info " + buttonBSClasses + " choice" + thePlayer + "' value='scissors'>Scissors</button>");
 
     if (thePlayer === 1) {
         player1Content.append(rock);
@@ -135,6 +146,40 @@ $(document).on("click", ".choice2", function () {
         });
 });
 
+// When player 1 quits the game, remove them from the database.
+$(document).on("click", "#quit1", function () {
+    database.ref('users/1').remove();
+});
+
+// When player 2 quits the game, remove them from the database.
+$(document).on("click", "#quit2", function () {
+    database.ref('users/2').remove();
+});
+
+// When a player quits the game, the board should be reset and a new player 
+// should be able to enter the game.
+database.ref('users').on("child_removed", function (snapshot) {
+    var playerRemoved = snapshot.val();
+
+    if (playerRemoved.playernumber === 1) {
+        player1Content.empty();
+        $("#player1-name").val("");
+        $("#player1-name").attr("readonly", false);
+        player1Button.removeClass("d-none");
+        player1Wins.text("");
+        player1Losses.text("");
+        $("#quit1").remove();
+    } else {
+        player2Content.empty();
+        $("#player2-name").val("");
+        $("#player2-name").attr("readonly", false);
+        player2Button.removeClass("d-none");
+        player2Wins.text("");
+        player2Losses.text("");
+        $("#quit2").remove();
+    }
+});
+
 // When a new player is added to the database, lock in their name and add the rock, paper,
 // scissors, and quit button options to the screen.
 database.ref('users').on("child_added", function (snapshot) {
@@ -144,21 +189,19 @@ database.ref('users').on("child_added", function (snapshot) {
     if (player.playernumber === 1) {
         $("#player1-name").val(player.playername);
         $("#player1-name").attr("readonly", true);
-        player1Button.addClass("disabled");
-        player1Button.attr("disabled", true);
+        player1Button.addClass("d-none");
         if (sessionID === player.playersessionID) {
             addRPSButtons(1);
-            var quitButton = $("<button class='btn btn-primary' id='quit'>Quit Game</button>");
+            var quitButton = $("<button class='btn-danger " + buttonBSClasses + "' id='quit1'>Quit Game</button>");
             player1Div.append(quitButton);
         }
     } else {
         $("#player2-name").val(player.playername);
         $("#player2-name").attr("readonly", true);
-        player2Button.addClass("disabled");
-        player2Button.attr("disabled", true);
+        player2Button.addClass("d-none");
         if (sessionID === player.playersessionID) {
             addRPSButtons(2);
-            var quitButton = $("<button class='btn btn-primary' id='quit'>Quit Game</button>");
+            var quitButton = $("<button class='btn-danger " + buttonBSClasses + "' id='quit2'>Quit Game</button>");
             player2Div.append(quitButton);
         }
     }
@@ -172,73 +215,90 @@ database.ref('users').on("child_added", function (snapshot) {
 database.ref('users').on("value", function (snapshot) {
     var users = snapshot.val();
 
+    // First make sure that we have users in the database, and that we have both a 
+    // player 1 and a player 2.
     if (users !== null && users[1] !== undefined && users[2] !== undefined) {
+
+        // Retrieve the players choices.
         var player1Choice = users[1].playerchoice;
         var player2Choice = users[2].playerchoice;
 
+        // Make sure that both players have made a choice.
         if (player1Choice !== "none" && player2Choice !== "none") {
-            deckDiv.empty();
-            deckDiv.append("<h4>Player 1 chose: " + player1Choice + "</h4>");
-            deckDiv.append("<h4>Player 2 chose: " + player2Choice + "</h4>");
 
+            // Display the players choices.
+            displayChoices(player1Choice, player2Choice);
+
+            // Compare the choices and determine who won the round.
             if (player1Choice === player2Choice) {
                 deckDiv.append("<h4>It was a tie!</h4>");
+                resetGameBoard();
             } else {
                 var didPlayer1Win = checkWinCondition(player1Choice, player2Choice);
                 if (didPlayer1Win) {
                     deckDiv.append("<h4>Player 1 won!</h4>");
-                    modifyWins(1);
-                    modifyLosses(2);
+                    modifyRecords("1","2");
                 } else {
                     deckDiv.append("<h4>Player 2 won!</h4>");
-                    modifyWins(2);
-                    modifyLosses(1);
+                    modifyRecords("2","1");
                 }
             }
+
+            // After a round, clear their choices from the database.
             clearChoices();
-            resetGameBoard();
         }
-        // } else {
-        //     console.log("Both players have not chosen yet.");
-        // }
     }
     // Handle the errors
 }, function (errorObject) {
     console.log("Errors handled: " + errorObject.code);
 });
 
+// Display the player choices as images and text.
+function displayChoices(player1, player2) {
+    deckDiv.empty();
+    player1Image.empty();
+    player2Image.empty();
+
+    if (player1 === "rock") {
+        player1Image.append(rockImage);
+    } else if (player1 === "scissors") {
+        player1Image.append(scissorsImage);
+    } else {
+        player1Image.append(paperImage);
+    }
+
+    if (player2 === "rock") {
+        player2Image.append(rockImage);
+    } else if (player2 === "scissors") {
+        player2Image.append(scissorsImage);
+    } else {
+        player2Image.append(paperImage);
+    }
+
+    deckDiv.append("<h4>Player 1 chose: " + player1 + "</h4>");
+    deckDiv.append("<h4>Player 2 chose: " + player2 + "</h4>");
+}
+
 // Check to see who won the round.
 function checkWinCondition(player1, player2) {
     if ((player1 === "rock" && player2 === "scissors") || (player1 === "scissors" && player2 === "paper") || (player1 === "paper" && player2 === "rock")) {
-        console.log("Player 1 won.");
         return true;
     } else {
-        console.log("Player 2 won.");
         return false;
     }
 }
 
-// Add 1 to the win count of the specified player.
-function modifyWins(thePlayer) {
-
-    // Get the current wins and add one to it.
+// Update the wins and losses of the players.
+function modifyRecords(winningPlayer, losingPlayer) {
     $.get('https://click-counter-72785.firebaseio.com/.json')
         .then(function (response) {
-            var wins = response.users[thePlayer].playerwins;
+            var wins = response.users[winningPlayer].playerwins;
             wins++;
-            database.ref('users/' + thePlayer + '/playerwins').update(wins);
-        });
-}
-
-// Add 1 to the loss count of the specified player.
-function modifyLosses(thePlayer) {
-
-    // Get the current losses and add one to it.
-    $.get('https://click-counter-72785.firebaseio.com/.json')
-        .then(function (response) {
-            var losses = response.users[thePlayer].playerlosses;
+            database.ref('users/' + winningPlayer + '/playerwins').set(wins);
+            var losses = response.users[losingPlayer].playerlosses;
             losses++;
-            database.ref('users/' + thePlayer + '/playerlosses').update(losses);
+            database.ref('users/' + losingPlayer + '/playerlosses').set(losses);
+            resetGameBoard();
         });
 }
 
@@ -248,6 +308,7 @@ function clearChoices() {
     database.ref('users/2/playerchoice').set("none");
 }
 
+// Empty the player notifications and reset the option buttons.
 function resetGameBoard() {
     sessionID = sessionStorage.getItem("sessionID");
 
@@ -265,5 +326,10 @@ function resetGameBoard() {
                 player2Content.empty();
                 addRPSButtons(2);
             }
+
+            player1Wins.text(response.users[1].playerwins);
+            player1Losses.text(response.users[1].playerlosses);
+            player2Wins.text(response.users[2].playerwins);
+            player2Losses.text(response.users[2].playerlosses);
         });
 }
